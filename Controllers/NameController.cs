0627275d047229e8b1027eacdcf3b61679f1c6de
camilarobledo <camilarobledo059@gmail.com>
2025-10-04 17:45:47 -0300

    Initial commit

---
+++ Controllers/NameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiPrimerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+
+        /// <summary>Devuelve un valor de prueba</summary>
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
